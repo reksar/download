39ea8d296310121e5cba8e0081dd7abf166628da
reksarka <reksarka@gmail.com>
2023-04-10 20:24:27 +0300

    Uses `tb` when ADODB is not available
    But the writing speed is too low in this case.

--- download.js
+++ download.js
@@ -19,6 +19,60 @@ try {
   bin_stream.SaveToFile(outfile);
   bin_stream.Close();
 } catch (err) {
-  // TODO: use `tb` to write response bytes.
-  throw err;
+  fs = new ActiveXObject("Scripting.FileSystemObject");
+  root = fs.GetParentFolderName(WScript.ScriptFullName);
+  tb(request.ResponseText);
+}
+
+
+function tb(text) {
+
+  // Import util.
+  var module = fs.BuildPath(root, "tb\\tests\\util.js");
+  var util = eval(fs.OpenTextFile(module).ReadAll());
+
+  var hexlines = new util.HexLineGenerator(new ByteGenerator(text));
+  var hex = new HexWriter(outfile);
+
+  while (!hexlines.Empty())
+    hex.Write(hexlines.Next());
+}
+
+
+function ByteGenerator(text) {
+
+  var i = 0;
+
+  this.Empty = function() {
+    return text.length <= i;
+  }
+
+  this.Next = function() {
+    return text.charCodeAt(i++);
+  }
+}
+
+
+function HexWriter(file) {
+
+  var shell = new ActiveXObject("WScript.Shell");
+  var tb = fs.BuildPath(root, "tb\\tb.bat");
+
+  // See https://learn.microsoft.com/en-us/previous-versions/d5fk67ky(v=vs.85)#remarks
+  var WindowStyle = {Hide: 0 /*...*/};
+
+  this.Write = function(hexline) {
+    var cmd = tb + " " + hexline + " " + file;
+    var rc = shell.Run(cmd, WindowStyle.Hide, /*wait*/true);
+    if (rc) Exit(rc);
+  }
+}
+
+
+function Exit(rc) {
+
+  if (rc && fs.FileExists(outfile))
+    fs.DeleteFile(outfile);
+
+  WScript.Quit(rc);
 }
